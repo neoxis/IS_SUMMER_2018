41443c7362de7521ad417b023390e900e64ccc72
Brad Stiff <bstiff@iastate.edu>
2018-06-03 23:21:37 -0500

    added List tab view for demo 1

--- ~demo_1/app/src/main/java/edu/bstiffiastate/cal_task/TasksActivity.java
+++ ~demo_1/app/src/main/java/edu/bstiffiastate/cal_task/TasksActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by bradj on 6/1/2018.
@@ -14,10 +16,26 @@ import android.view.ViewGroup;
 
 public class TasksActivity extends Fragment
 {
+    private ListView todo_tasks, g_list;
+
+    String[] t_l = {"Task 1","Task 2","Task 3"};
+    String[] g_l = {"Item 1","Item 2","Item 3"};
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.tasks_fragment, container, false);
+
+        //Fill Tasks for demo
+        ArrayAdapter<String> tList = new ArrayAdapter<String>(getActivity(),R.layout.task_list_item,R.id.task_title,t_l);
+        todo_tasks = rootView.findViewById(R.id.todo_list);
+        todo_tasks.setAdapter(tList);
+
+        //Fill Today Events for demo
+        ArrayAdapter<String> todayE = new ArrayAdapter<String>(getActivity(),R.layout.grocery_list_item,R.id.grocery_item,g_l);
+        g_list = rootView.findViewById(R.id.grocery_list);
+        g_list.setAdapter(todayE);
+
         return rootView;
     }
 }
