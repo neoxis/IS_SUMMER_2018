c6254ddc19c5df62313181feb67d1fb5b57a6948
Brad Stiff <bstiff@iastate.edu>
2018-07-18 02:12:12 -0500

    fixed todayActivity not loading after going to calendar tab

--- demo_2/app/src/main/java/edu/bstiffiastate/caltask/MainActivity.java
+++ demo_2/app/src/main/java/edu/bstiffiastate/caltask/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(2);
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
